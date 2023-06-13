14072a9d9ccbb4b8a6f4d7c5fe36af2729ec7321
Josue S <josuesandoval02@gmail.com>
2023-06-13 15:46:49 -0400

    Remove URLPrepareer.py and incorporate into codeScrapper.py

--- codeScrapper.py
+++ codeScrapper.py
@@ -2,16 +2,27 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
-df = pd.read_csv('processedURL-concat.csv')
+df = pd.read_csv('./file-history/concatenated.csv')
+
+def prepareUrl(url): #reformats HTML-URL to redirect to raw code for scraping
+    out = ''
+    out = url.replace('github.com', 'raw.githubusercontent.com')
+    out = out.replace('/blob', '')
+    return out
+
+df['RAW'] = df['HTML-URL'].apply(prepareUrl)
 
 urlList = df['RAW']
+print('URL editing complete')
 
-def fetchCode(url):
+def fetchCode(url): #Calls BeautifulSoup to grab code from altered URL
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     return soup.prettify()
         
 print('start')
-df['CODE'] = urlList.apply(fetchCode) #1108 Entries will take ~220 Seconds to fetch all data
+df['CODE'] = urlList.apply(fetchCode) 
+#1108 Entries will take ~220 Seconds to fetch all data (WILL VARY ON CONNECTION SPEED)
 
-df.to_csv('finalTable.csv')
+df.to_csv('./file-history/finalTable.csv')
+print('finalTable.csv created')
